364716a11f71d06ce1a5ceb9a85b0b043d0490e9
Oditya26 <151801552+Oditya26@users.noreply.github.com>
2024-02-19 08:46:27 +0700

    Maintance Update

--- Ojek Online/src/aplikasi_ojek_online/Aplikasi.java
+++ Ojek Online/src/aplikasi_ojek_online/Aplikasi.java
@@ -23,12 +23,17 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import org.w3c.dom.events.MouseEvent;
 import javax.swing.table.TableColumnModel;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
 
 /**
  *
  * @author oditr
  */
 public class Aplikasi extends javax.swing.JFrame {
+
+
+
+
 
     /**
      * Creates new form Aplikasi
@@ -418,6 +423,7 @@ public class Aplikasi extends javax.swing.JFrame {
         JTableHeader defaultHeader = tabelData.getTableHeader();
         defaultHeader.setBackground(new Color(245, 246, 250));
         tampilkan_data_overview();
+        
     }
 
     /**
@@ -429,8 +435,6 @@ public class Aplikasi extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
@@ -440,33 +444,12 @@ public class Aplikasi extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tabelData = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Background Color");
-
-        jPanel1.setBackground(new java.awt.Color(69, 115, 255));
-        jPanel1 = new GradientPanel();
-
-        jLabel1.setFont(new java.awt.Font("Lato", 1, 24)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(245, 246, 250));
-        jLabel1.setText("View Database Ojek Online");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(51, 51, 51)
-                .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(26, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(25, 25, 25))
-        );
+        setTitle("Ojek Online");
+        setMaximumSize(new java.awt.Dimension(875, 600));
+        setResizable(false);
 
         jLabel2.setFont(new java.awt.Font("Lato", 1, 18)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(44, 53, 77));
@@ -479,7 +462,7 @@ public class Aplikasi extends javax.swing.JFrame {
         javax.swing.JButton jButton1 = new CustomButton("Overview\nDatabase");
         jButton1.setFont(new java.awt.Font("Lato", 1, 18)); // NOI18N
         jButton1.setForeground(new java.awt.Color(44, 53, 77));
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/Overview.png"))); // NOI18N
+        jButton1.setIcon(new FlatSVGIcon("Icons/Overview.svg", 40,40));
         jButton1.setText("<html><center>Overview<br>Database</center></html>");
         jButton1.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
         jButton1.setBorderPainted(false);
@@ -493,7 +476,7 @@ public class Aplikasi extends javax.swing.JFrame {
         javax.swing.JButton jButton2 = new CustomButton("List\nBooking");
         jButton2.setFont(new java.awt.Font("Lato", 1, 18)); // NOI18N
         jButton2.setForeground(new java.awt.Color(44, 53, 77));
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/Ticket.png"))); // NOI18N
+        jButton2.setIcon(new FlatSVGIcon("Icons/Ticket.svg", 40,40));
         jButton2.setText("<html><left>List<br>Booking</left></html>");
         jButton2.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -505,8 +488,8 @@ public class Aplikasi extends javax.swing.JFrame {
         javax.swing.JButton jButton3 = new CustomButton("List\nDriver   ");
         jButton3.setFont(new java.awt.Font("Lato", 1, 18)); // NOI18N
         jButton3.setForeground(new java.awt.Color(44, 53, 77));
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/Car.png"))); // NOI18N
-        jButton3.setText("<html><left>List<br>Driver   </left></html>");
+        jButton3.setIcon(new FlatSVGIcon("Icons/Car.svg", 43,40));
+        jButton3.setText("<html><left>List<br> Driver   </left></html>");
         jButton3.setAlignmentY(0.0F);
         jButton3.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
         jButton3.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
@@ -520,7 +503,7 @@ public class Aplikasi extends javax.swing.JFrame {
         javax.swing.JButton jButton4 = new CustomButton("List\nPassenger");
         jButton4.setFont(new java.awt.Font("Lato", 1, 18)); // NOI18N
         jButton4.setForeground(new java.awt.Color(44, 53, 77));
-        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/People.png"))); // NOI18N
+        jButton4.setIcon(new FlatSVGIcon("Icons/People.svg", 40,40));
         jButton4.setText("<html><left>List<br>Passenger</left></html>");
         jButton4.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
         jButton4.addActionListener(new java.awt.event.ActionListener() {
@@ -565,28 +548,33 @@ public class Aplikasi extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
+        jLabel1.setIcon(new FlatSVGIcon("Icons/Home.svg", 879,202));
+        jLabel1.setMaximumSize(new java.awt.Dimension(353, 81));
+        jLabel1.setMinimumSize(new java.awt.Dimension(353, 81));
+        jLabel1.setPreferredSize(new java.awt.Dimension(353, 81));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addGap(51, 51, 51)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, 148, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 148, Short.MAX_VALUE)
                     .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 148, Short.MAX_VALUE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(51, Short.MAX_VALUE))
+                .addContainerGap(55, Short.MAX_VALUE))
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
@@ -602,7 +590,7 @@ public class Aplikasi extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(72, Short.MAX_VALUE))
+                .addContainerGap(45, Short.MAX_VALUE))
         );
 
         pack();
@@ -672,7 +660,6 @@ public class Aplikasi extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tabelData;
